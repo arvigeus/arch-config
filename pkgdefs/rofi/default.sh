099253c93d38200668173b3cd8704e32b01f6abb
Nikolay Stoynov <arvigeus@gmail.com>
2025-02-28 11:48:08 +0700

    rofi_theme.sh

--- pkgdefs/rofi/default.sh
+++ pkgdefs/rofi/default.sh
@@ -2,6 +2,15 @@
 
 sudo pacman -S --needed --noconfirm rofi-wayland
 
+# Theme based on windows11-list-dark.rasi from https://github.com/newmanls/rofi-themes-collection
+mkdir -p "$HOME/.local/share/rofi/themes/"
+cp -u -p ./pkgdefs/rofi/theme.rasi "$HOME/.local/share/rofi/themes/list-theme.rasi"
+mkdir -p "$HOME/.config/rofi"
+{
+  echo '@theme "'"$HOME/.local/share/rofi/themes/list-theme.rasi"'"'
+  echo 'configuration { scroll-method: 1; }'
+} > "$HOME/.config/rofi/config.rasi"
+
 source ./pkgdefs/rofi/polkit.sh
 
 mkdir -p "$HOME/.local/bin/"
