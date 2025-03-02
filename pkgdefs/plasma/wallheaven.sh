3527abf7e7b2de2fcbc3f97dfa50cf8f4e1662ef
Nikolay Stoynov <arvigeus@gmail.com>
2025-03-03 00:46:13 +0700

    wallheaven.sh

--- pkgdefs/plasma/wallheaven.sh
+++ pkgdefs/plasma/wallheaven.sh
@@ -2,43 +2,41 @@
 
 APPLETSRC_CONFIG="$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
 
-# Set Wallhaven as the wallpaper plugin using qdbus
-qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
-    var Desktops = desktops();
-    for (var i = 0; i < Desktops.length; i++) {
-        var d = Desktops[i];
-        d.wallpaperPlugin = 'com.plasma.wallpaper.wallhaven'; // Set Wallhaven as wallpaper engine
-        d.currentConfigGroup = ['Wallpaper', 'com.plasma.wallpaper.wallhaven', 'General'];
-        d.writeConfig('Image', '');
-    }
-"
+# Get a list of all containments using grep and regex
+# This extracts all containment IDs from section headers that match [Containments][X]
+containment_ids=$(grep -o '^\[Containments\]\[[0-9]\+\]$' "$APPLETSRC_CONFIG" | grep -o '[0-9]\+')
 
-CONTAINMENT_ID=""
-CURRENT_CONTAINMENT=""
-
-# Read the config file line by line
-while IFS= read -r line; do
-    if [[ "$line" =~ ^\[Containments\]\[([0-9]+)\]$ ]]; then
-        CURRENT_CONTAINMENT="${BASH_REMATCH[1]}"
-    elif [[ "$line" == "wallpaperplugin=com.plasma.wallpaper.wallhaven" ]]; then
-        CONTAINMENT_ID="$CURRENT_CONTAINMENT"
-        break
+for id in $containment_ids; do
+    wallpaper_plugin=$(kreadconfig6 --file "$APPLETSRC_CONFIG" \
+                     --group "Containments" --group "$id" \
+                     --key "wallpaperplugin" 2>/dev/null)
+    
+    plugin=$(kreadconfig6 --file "$APPLETSRC_CONFIG" \
+                       --group "Containments" --group "$id" \
+                       --key "plugin" 2>/dev/null)
+    
+    if [ -n "$wallpaper_plugin" ] && { [ "$plugin" = "org.kde.plasma.folder" ] || [ "$plugin" = "org.kde.desktopcontainment" ]; }; then
+        kwriteconfig6 --file "$APPLETSRC_CONFIG" \
+                     --group "Containments" --group "$id" \
+                     --key "plugin" "org.kde.desktopcontainment"
+                     
+        kwriteconfig6 --file "$APPLETSRC_CONFIG" \
+                     --group "Containments" --group "$id" \
+                     --key "wallpaperplugin" "com.plasma.wallpaper.wallhaven"
+        
+        kwriteconfig6 --file "$APPLETSRC_CONFIG" \
+                     --group "Containments" --group "$id" \
+                     --group "Wallpaper" --group "com.plasma.wallpaper.wallhaven" \
+                     --group "General" --key "Query" \
+                     "landscape mountains"
+        
+        kwriteconfig6 --file "$APPLETSRC_CONFIG" \
+                     --group "Containments" --group "$id" \
+                     --group "Wallpaper" --group "com.plasma.wallpaper.wallhaven" \
+                     --group "General" --key "WallpaperDelay" \
+                     "1800"
     fi
-done < "$APPLETSRC_CONFIG"
+done
 
-if [[ -n "$CONTAINMENT_ID" ]]; then
-    kwriteconfig6 --file "$APPLETSRC_CONFIG" \
-                  --group "Containments" --group "$CONTAINMENT_ID" \
-                  --group "Wallpaper" --group "com.plasma.wallpaper.wallhaven" \
-                  --group "General" --key "Query" \
-                  "Makoto Shinkai,Kimi no Na Wa,The Garden of Words,5 Centimeters Per Second,Studio Ghibli,Nier Automata,Nier,Life is Strange,Wall-E,Bioshock Infinite,Stellar Blade,Firewatch,Transistor,Erased"
-
-    kwriteconfig6 --file "$APPLETSRC_CONFIG" \
-                  --group "Containments" --group "$CONTAINMENT_ID" \
-                  --group "Wallpaper" --group "com.plasma.wallpaper.wallhaven" \
-                  --group "General" --key "WallpaperDelay" \
-                  "1800"
-
-    # Restart plasmashell silently
-    ( sleep 1 && kquitapp6 plasmashell && sleep 2 && plasmashell --replace & ) >/dev/null 2>&1 &
-fi
+# Restart plasmashell to apply changes
+( sleep 1 && kquitapp6 plasmashell && sleep 2 && plasmashell --replace & ) >/dev/null 2>&1 &
