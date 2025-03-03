afdfbb7591279ec21dfab8d1340b211726b3e09e
Nikolay Stoynov <arvigeus@gmail.com>
2025-03-03 11:16:05 +0700

    chromium_sync.sh

--- pkgdefs/chromium/default.sh
+++ pkgdefs/chromium/default.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+source ./utils/file.sh
+
 sudo pacman -S --needed --noconfirm chromium
 source ./pkgdefs/chromium/extensions.sh
+
+# TODO: Policies: https://github.com/arvigeus/nixos-config/blob/master/apps/chromium.nix
+
+# Allow Google Sync for Chromium
+# These keys are from public web
+file_add_line "GOOGLE_DEFAULT_CLIENT_ID=77185425430.apps.googleusercontent.com" "$HOME/.env"
+file_add_line "GOOGLE_DEFAULT_CLIENT_SECRET=OTJgUOQcT7lO7GsGZq2G4IlT" "$HOME/.env"
--- utils/file.sh
+++ utils/file.sh
@@ -7,12 +7,9 @@ file_add_line() {
     # Ensure the file exists before doing anything
     touch "$file"
 
-    # Escape special characters for grep (using printf for safety)
-    local escaped_line
-    escaped_line=$(printf "%s\n" "$line" | sed 's/[.[\*^$]/\\&/g')
-
     # Check if the exact line exists
-    if ! grep -Fxq -- "$escaped_line" "$file"; then
-        echo "$line" >> "$file"
+    if ! grep -Fxq -- "$line" "$file"; then
+        echo "$line" >>"$file"
     fi
+
 }
