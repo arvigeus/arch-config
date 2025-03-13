bf3c6271772557583259ba45395c1d1384d6e50f
Nikolay Stoynov <arvigeus@gmail.com>
2025-03-13 12:09:17 +0700

    warp_local.sh

--- pkgdefs/cloudflare/README.md
+++
@@ -1,5 +0,0 @@
-# Cloudflare
-
-## Issues
-
-- KDE Connect does not work when WARP is active. To connect, disable it first: `warp-cli disconnect`.
--- pkgdefs/cloudflare/default.sh
+++ pkgdefs/cloudflare/default.sh
@@ -2,6 +2,20 @@
 
 paru -S --noconfirm --needed cloudflare-warp-bin
 sudo systemctl enable warp-svc.service --now
+
 sleep 1
+
+# Add local network ranges to split tunnel
+# This allows apps like KDE Connect to function
+warp-cli tunnel ip add-range 192.168.0.0/16
+warp-cli tunnel ip add-range 172.16.0.0/12
+warp-cli tunnel ip add-range 10.0.0.0/8
+# Check configuration
+# warp-cli tunnel ip list
+# Remove range
+# warp-cli tunnel ip remove-range 192.168.0.0/16
+# warp-cli tunnel ip remove-range 172.16.0.0/12
+# warp-cli tunnel ip remove-range 10.0.0.0/8
+
 warp-cli registration new
 warp-cli connect
