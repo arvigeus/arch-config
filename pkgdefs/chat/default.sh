981643027dce4f0d27f5df0b7945d6ed71ddbb9e
Nikolay Stoynov <arvigeus@gmail.com>
2025-03-15 23:52:56 +0700

    plasma_stuff.sh

--- pkgdefs/chat/default.sh
+++ pkgdefs/chat/default.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#flatpak install -y flathub com.rtosta.zapzap
+flatpak install -y flathub com.rtosta.zapzap
 #flatpak install -y flathub com.ktechpit.whatsie
 #flatpak install -y flathub org.telegram.desktop
 #flatpak install -y flathub com.discordapp.Discord
--- pkgdefs/flatpak/default.sh
+++ pkgdefs/flatpak/default.sh
@@ -2,4 +2,6 @@
 
 sudo pacman -S --needed --noconfirm flatpak xdg-desktop-portal-kde
 
+sudo pacman -S --needed --noconfirm flatpak-kcm 
+
 source ./pkgdefs/flatpak/repos.sh
