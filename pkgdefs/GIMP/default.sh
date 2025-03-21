cbd52c54f9f1717835e4f19db15f9dab7fcee211
Nikolay Stoynov <arvigeus@gmail.com>
2025-03-21 13:54:23 +0700

    gimp3.sh

--- pkgdefs/GIMP/default.sh
+++ pkgdefs/GIMP/default.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Check for newer versions:
+# flatpak search org.gimp.GIMP
+
+# TODO: Install missing plugins once they are available
 flatpak install -y flathub org.gimp.GIMP
-flatpak install -y flathub org.gimp.GIMP.Plugin.Resynthesizer/x86_64/2-40
-flatpak install -y flathub org.gimp.GIMP.Plugin.Lensfun/x86_64/2-40
-flatpak install -y flathub org.gimp.GIMP.Plugin.Fourier/x86_64/2-40
-flatpak install -y flathub org.gimp.GIMP.Plugin.BIMP/x86_64/2-40
-flatpak install -y flathub org.gimp.GIMP.Plugin.LiquidRescale/x86_64/2-40
-flatpak install -y flathub org.gimp.GIMP.Plugin.FocusBlur/x86_64/2-40
-flatpak install -y flathub org.gimp.GIMP.Plugin.GMic/x86_64/2-40
+flatpak install -y flathub org.gimp.GIMP.Plugin.Resynthesizer/x86_64/3
+#flatpak install -y flathub org.gimp.GIMP.Plugin.Lensfun/x86_64/3
+flatpak install -y flathub org.gimp.GIMP.Plugin.Fourier/x86_64/3
+#flatpak install -y flathub org.gimp.GIMP.Plugin.BIMP/x86_64/3
+#flatpak install -y flathub org.gimp.GIMP.Plugin.LiquidRescale/x86_64/3
+#flatpak install -y flathub org.gimp.GIMP.Plugin.FocusBlur/x86_64/3
+flatpak install -y flathub org.gimp.GIMP.Plugin.GMic/x86_64/3
+
