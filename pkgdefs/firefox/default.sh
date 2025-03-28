c82f6075ca3c0fa741c7698b65ebeb81a4da7e7a
Nikolay Stoynov <arvigeus@gmail.com>
2025-03-28 14:43:39 +0700

    firefox_plasma_integration.sh

--- pkgdefs/firefox/default.sh
+++ pkgdefs/firefox/default.sh
@@ -6,3 +6,6 @@ source ./pkgdefs/browser-extensions/default.sh
 install_browser_extensions "firefox"
 
 # TODO: Settings: https://github.com/arvigeus/nixos-config/blob/master/apps/firefox.nix
+
+# TODO: Enable plasma integration
+# about:config: widget.use-xdg-desktop-portal.file-picker 1
