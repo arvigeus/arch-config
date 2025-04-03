5fb0734a2bdd347ed6b7b2482628eacb636fa5b2
Nikolay Stoynov <arvigeus@gmail.com>
2025-04-03 10:53:43 +0700

    misc.sh

--- pkgdefs/gnome/default.sh
+++ pkgdefs/gnome/default.sh
@@ -9,3 +9,5 @@ flatpak install -y flathub com.github.neithern.g4music
 flatpak install -y flathub com.rafaelmardojai.Blanket
 
 flatpak install -y flathub com.github.johnfactotum.Foliate
+
+flatpak install -y flathub de.swsnr.pictureoftheday
---
+++ pkgdefs/steam/Games.md
@@ -0,0 +1,5 @@
+# Games
+
+## Age of Empires
+
+- [ageLANServer](https://github.com/luskaner/ageLANServer) - Web Server for AoE 1, 2 and 3 DE supporting LAN multiplayer 100% offline.
