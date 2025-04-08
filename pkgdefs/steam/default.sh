e41a5039cac506513d0475e2beeefb3f9ad7f368
Nikolay Stoynov <arvigeus@gmail.com>
2025-04-08 10:36:14 +0700

    updates.sh

--- pkgdefs/steam/default.sh
+++ pkgdefs/steam/default.sh
@@ -42,28 +42,29 @@ sudo pacman -S --needed --noconfirm steamtinkerlaunch
 source ./pkgdefs/steam/add-to-steam/default.sh
 
 create_steam_minimal_shortcut() {
-  local Mode=$1
-  local mode="${1,,}"
+  local exec_path=$1
+  local suffix=$2
+  local display_name=$3
 
-  local desktop_file="$HOME/.local/share/applications/steam_minimal_${mode}.desktop"
+  local desktop_dir="$HOME/.local/share/applications"
+  local desktop_file="${desktop_dir}/steam_minimal${suffix:+_${suffix}}.desktop"
 
-  if [ ! -e "$desktop_file" ]; then
-    mkdir -p "$HOME/.local/share/applications"
-    cat > "$desktop_file" <<EOF
+  mkdir -p "$desktop_dir"
+
+  cat > "$desktop_file" <<EOF
 [Desktop Entry]
 Type=Application
-Name=Steam Minimal (${Mode})
-Exec=/usr/bin/steam-${mode} -cef-disable-gpu-compositing -cef-disable-gpu steam://open/minigameslist %U
+Name=${display_name}
+Exec=${exec_path} -cef-disable-gpu-compositing -cef-disable-gpu steam://open/minigameslist %U
 Categories=Network;FileTransfer;Game;
 Icon=steam
 Terminal=false
 MimeType=x-scheme-handler/steam;x-scheme-handler/steamlink;
 EOF
-  fi
 }
 
-create_steam_minimal_shortcut "Runtime"
-create_steam_minimal_shortcut "Native"
+create_steam_minimal_shortcut "/usr/bin/steam" "" "Steam Minimal"
+create_steam_minimal_shortcut "/usr/bin/steam-native" "native" "Steam Minimal (Native)"
 
 # https://wiki.archlinux.org/title/Improving_performance
 echo "vm.max_map_count = 2147483642" | sudo tee /etc/sysctl.d/80-gamecompatibility.conf
--- pkgdefs/vscode/default.sh
+++ pkgdefs/vscode/default.sh
@@ -2,5 +2,6 @@
 
 # https://wiki.archlinux.org/title/Visual_Studio_Code
 sudo pacman -S --needed --noconfirm code
+#paru -S --needed --noconfirm code-features
 source ./pkgdefs/vscode/extensions.sh
 cp -u -p ./pkgdefs/vscode/settings.json "$HOME/.config/Code - OSS/User/settings.json"
