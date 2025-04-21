8ec39bf807f401fc3ec0ab5ef47bad4f3e5228f8
Nikolay Stoynov <arvigeus@gmail.com>
2025-04-21 11:40:48 +0700

    zed_toggle_terminal.sh

--- pkgdefs/zed/default.sh
+++ pkgdefs/zed/default.sh
@@ -3,3 +3,4 @@
 sudo pacman -S --needed --noconfirm zed
 mkdir -p "$HOME/.config/zed"
 cp -u -p ./pkgdefs/zed/settings.json "$HOME/.config/zed/settings.json"
+cp -u -p ./pkgdefs/zed/keymap.jsonc "$HOME/.config/zed/keymap.json"
