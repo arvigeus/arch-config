da04c0c21f0022f3cdc1c5b6279e659b6a17c5a1
Nikolay Stoynov <arvigeus@gmail.com>
2025-04-21 14:22:31 +0700

    rocm.sh

--- pkgdefs/amdgpu/default.sh
+++ pkgdefs/amdgpu/default.sh
@@ -5,6 +5,10 @@
 sudo pacman -S --needed --noconfirm amd-ucode mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon amdvlk lib32-amdvlk mesa lib32-mesa vulkan-icd-loader lib32-vulkan-icd-loader vulkan-mesa-layers lib32-vulkan-mesa-layers fwupd
 sudo pacman -S --needed --noconfirm rocm-opencl-runtime rocm-hip-runtime
 
+# Add the current user to the render group so the user has permission to run compute workloads (ROCm)
+# https://rocm.docs.amd.com/projects/install-on-linux/en/latest/
+sudo usermod -aG render "$USER"
+
 mkdir -p "$HOME/.config/environment.d"
 cp -u -p ./pkgdefs/amdgpu/env.conf "$HOME/.config/environment.d/vulkan.conf"
 systemctl --user daemon-reexec
