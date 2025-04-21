2e7a684065fe2fa0b0c4de2e7672c0b58b85cb2c
Nikolay Stoynov <arvigeus@gmail.com>
2025-04-21 11:32:20 +0700

    rustup.sh

--- pkgdefs/rust/default.sh
+++ pkgdefs/rust/default.sh
@@ -3,5 +3,9 @@
 sudo pacman -S --needed --noconfirm rust
 
 # sudo pacman -Rns rust
-# sudo pacman -S --needed --noconfirm rust
+# sudo pacman -S --needed --noconfirm rustup
 # rustup default stable
+# rustup component add clippy
+# rustup component add rustfmt
+# rustup component add rust-analyzer
+# rustup component add rust-src
