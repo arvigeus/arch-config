493190f5fa4fe2a4ac3f811e9ca14bcf3c0c2b01
Nikolay Stoynov <arvigeus@gmail.com>
2025-04-21 16:22:41 +0700

    arkenfox_and_settings.sh

--- pkgdefs/firefox/default.sh
+++ pkgdefs/firefox/default.sh
@@ -2,10 +2,26 @@
 
 sudo pacman -S --needed --noconfirm firefox
 
-source ./pkgdefs/browser-extensions/default.sh
-install_browser_extensions "firefox"
-
 # TODO: Settings: https://github.com/arvigeus/nixos-config/blob/master/apps/firefox.nix
 
-# TODO: Enable plasma integration
-# about:config: widget.use-xdg-desktop-portal.file-picker 1
+sudo pacman -S --needed --noconfirm arkenfox-user.js
+
+PROFILE_DIR=$(grep 'Path=' ~/.mozilla/firefox/profiles.ini | sed 's/^Path=//' | head -n1)
+FULL_PROFILE_PATH="${HOME}/.mozilla/firefox/${PROFILE_DIR}"
+# Create user-overrides.js with custom prefs
+[[ -f "${FULL_PROFILE_PATH}/user-overrides.js" ]] || install -Dm644 /dev/stdin "${FULL_PROFILE_PATH}/user-overrides.js" <<'EOF'
+// Enable restoring previous session
+user_pref("browser.startup.page", 3);
+user_pref("browser.sessionstore.enabled", true);
+user_pref("browser.sessionstore.privacy_level", 0);
+
+// Use native XDG file picker
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
+
+user_pref("dom.webgpu.enabled", true);
+EOF
+
+arkenfox-updater -s
+
+source ./pkgdefs/browser-extensions/default.sh
+install_browser_extensions "firefox"
