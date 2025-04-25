737dcc5df4db5c3cb78c33c1c64ef89744096024
Nikolay Stoynov <arvigeus@gmail.com>
2025-04-26 00:54:48 +0700

    steam_big_picture_disable_ryzenadj.sh

--- pkgdefs/gamescope/session/start.sh
+++ pkgdefs/gamescope/session/start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-set -v
-set -e  # Exit on error
+# set -v
+# set -e  # Exit on error
 
 # Enable logging
-exec > ~/gamescope.log 2>&1
+#exec > ~/gamescope.log 2>&1
 
 # shellcheck disable=SC3045
 ulimit -n 524288 || echo "Failed to set ulimit"
@@ -12,6 +12,7 @@ sudo systemctl start plugin_loader.service
 
 asusctl profile -P Performance
 
+# Generally this doesn't work: neither Geamescope session, neither Steam Big Picture
 #sudo ryzenadj --tctl-temp=80
 
 switcherooctl launch gamescope \
@@ -21,3 +22,8 @@ switcherooctl launch gamescope \
   -- steam-native -steamos3 -steampal -steamdeck -gamepadui -pipewire-dmabuf
   # -O 'DP-3,DP-1,HDMI-A-1,*,eDP-1' -r 75
   # --generate-drm-mode --hdr-enabled --hdr-itm-enable
+
+gamescope_pid="$!"
+kill -9 $gamescope_pid
+# shellcheck disable=SC3045
+wait -n "$gamescope_pid"
