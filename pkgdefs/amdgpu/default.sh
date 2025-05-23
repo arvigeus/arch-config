39e51321f50004456b3a419a4b16f7a376680294
Nikolay Stoynov <arvigeus@gmail.com>
2025-05-24 00:15:42 +0700

    monitoring_tools.sh

--- pkgdefs/amdgpu/default.sh
+++ pkgdefs/amdgpu/default.sh
@@ -5,6 +5,9 @@
 sudo pacman -S --needed --noconfirm amd-ucode mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon amdvlk lib32-amdvlk mesa lib32-mesa vulkan-icd-loader lib32-vulkan-icd-loader vulkan-mesa-layers lib32-vulkan-mesa-layers fwupd
 sudo pacman -S --needed --noconfirm rocm-opencl-runtime rocm-hip-runtime
 
+# Monitoring tools
+sudo pacman -S --needed --noconfirm radeontop nvtop
+
 # Add the current user to the render group so the user has permission to run compute workloads (ROCm)
 # https://rocm.docs.amd.com/projects/install-on-linux/en/latest/
 sudo usermod -aG render "$USER"
--- pkgdefs/utils/default.sh
+++ pkgdefs/utils/default.sh
@@ -2,3 +2,6 @@
 
 # bind: allows dig
 sudo pacman -S --needed --noconfirm jq bc evtest whois bind wget less rsync rclone
+
+# wireless device monitoring
+sudo pacman -S --needed --noconfirm wavemon
