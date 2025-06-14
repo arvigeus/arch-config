b7097affbf0fb87d3e91bbd547865e19280b7751
Nikolay Stoynov <arvigeus@gmail.com>
2025-06-14 20:06:19 +0700

    subtitle_composer.sh

--- pkgdefs/media/default.sh
+++ pkgdefs/media/default.sh
@@ -4,6 +4,8 @@ sudo pacman -S --needed --noconfirm ffmpeg mkvtoolnix-cli
 
 sudo pacman -S --needed --noconfirm imagemagick ghostscript
 
+flatpak install -y flathub org.kde.subtitlecomposer
+
 flatpak install -y flathub dev.aunetx.deezer
 
 flatpak install -y flathub org.bunkus.mkvtoolnix-gui
