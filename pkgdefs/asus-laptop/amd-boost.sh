d4255519c119d23f2f9053f8e26ab71c76998e8a
Nikolay Stoynov <arvigeus@gmail.com>
2025-06-15 00:44:55 +0700

    amd_boost_info.sh

--- pkgdefs/asus-laptop/amd-boost.sh
+++ pkgdefs/asus-laptop/amd-boost.sh
@@ -3,8 +3,10 @@
 set -euo pipefail
 
 if [[ $# -ne 1 || "$1" != @(0|1) ]]; then
-    echo "Usage: $0 <0|1>  # 0 to disable boost, 1 to enable" >&2
-    exit 1
+    echo -e "Usage: $0 <0|1>  # 0 to disable boost, 1 to enable\n" >&2
+    # Check state
+    cpupower frequency-info
+    exit 0
 fi
 
 STATE="$1"
@@ -16,6 +18,3 @@ for cpu_path in /sys/devices/system/cpu/cpu[0-9]*/cpufreq/boost; do
         echo "Missing: $cpu_path" >&2
     fi
 done
-
-# Check state
-# cpupower frequency-info
