be220b591d3633d5f3320f1b6ee58fdb4243c5d0
Nikolay Stoynov <arvigeus@gmail.com>
2025-11-09 22:08:11 +0200

    meld_diffprog.sh

--- pkgdefs/delta/default.sh
+++ pkgdefs/delta/default.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 sudo pacman -S --needed --noconfirm git-delta diffutils
-
-mkdir -p "$HOME/.config/environment.d"
-cp -u -p ./pkgdefs/delta/env.conf "$HOME/.config/environment.d/delta.conf"
-systemctl --user daemon-reexec
--- pkgdefs/meld/default.sh
+++ pkgdefs/meld/default.sh
@@ -6,3 +6,10 @@ sudo pacman -S --needed --noconfirm meld
 # dconf reset -f /org/gnome/meld/
 
 dconf load /org/gnome/meld/ < ./pkgdefs/meld/settings.conf
+
+mkdir -p "$HOME/.config/environment.d"
+cp -u -p ./pkgdefs/meld/env.conf "$HOME/.config/environment.d/meld.conf"
+systemctl --user daemon-reexec
+
+mkdir -p "$HOME/.config/arch-update"
+echo 'DiffProg=meld' >> "$HOME/.config/arch-update/arch-update.conf"
