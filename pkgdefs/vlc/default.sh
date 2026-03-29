ed9c8edbaae16e50dd735c5fc6901a2079538957
Nikolay Stoynov <arvigeus@gmail.com>
2026-03-29 20:34:37 +0700

    vlc_plugins.sh

--- pkgdefs/vlc/default.sh
+++ pkgdefs/vlc/default.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # https://wiki.archlinux.org/title/VLC_media_player
-sudo pacman -S --needed --noconfirm vlc
+sudo pacman -S --needed --noconfirm vlc vlc-plugins-all
+
+paru -S --noconfirm --needed vlc-pause-click-plugin
+
+VLCRC="$HOME/.config/vlc/vlcrc"
+mkdir -p "$(dirname "$VLCRC")" && touch "$VLCRC"
+crudini --set "$VLCRC" core video-filter pause_click
+crudini --set "$VLCRC" core control pause_click
