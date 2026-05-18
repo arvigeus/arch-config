bbe463e5d92b8177dcba5be265295d6929d8bf3a
Nikolay Stoynov <arvigeus@gmail.com>
2026-05-18 11:37:39 +0700

    virt-manager_ufw.sh

--- pkgdefs/virtualization/default.sh
+++ pkgdefs/virtualization/default.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 sudo pacman -S --needed --noconfirm virt-manager
+
+sudo ufw allow in on virbr0
+sudo ufw route allow in on virbr0 out on wlan0
+sudo ufw route allow out on virbr0
+sudo sysctl net.ipv4.ip_forward=1
+echo 'net.ipv4.ip_forward=1' | sudo tee /etc/sysctl.d/99-libvirt.conf
